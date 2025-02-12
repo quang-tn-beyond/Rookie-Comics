f13485541fbc046804e2d9ba643744df46fd5bd0
quang-tn-beyond <trannhatquang0908@gmail.com>
2025-02-13 01:40:36 +0700

    a

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,5 @@
 
 import { Link } from "react-router-dom";
-import logo from "src/assets/img/logo.png";
 
 export default function Header() {
     return (
@@ -10,7 +9,7 @@ export default function Header() {
                     <div className="col-lg-2">
                         <div className="header__logo">
                             <a href="./">
-                            <img src={logo} alt="logo" />;
+                            <img src="/assets/img/logo.png" alt="logo" />
                             </a>
                         </div>
                     </div>
