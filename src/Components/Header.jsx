33899c13c7e048eb789bb9b1ff32c6ba16f3a885
quang-tn-beyond <trannhatquang0908@gmail.com>
2025-02-13 01:32:36 +0700

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
                     <div className="col-lg-2">
                         <div className="header__logo">
                             <a href="./">
-                            <img src="src/assets/logo.png" alt="logo" />
+                            <img src="src/assets/img/logo.png" alt="logo" />
                             </a>
                         </div>
                     </div>
